b49b2fdbf07f6e690efe89f824b3f11645cdd057
Kyle Stevenson <ky_steve@hotmail.com>
2016-12-04 22:02:57 -0500

    added comments,

--- Services/checkPostalCheck/checkPostalCheck/PostalCodeValidator.h
+++ Services/checkPostalCheck/checkPostalCheck/PostalCodeValidator.h
@@ -1,3 +1,9 @@
+// Program: SOA
+// Assingment 1
+// Date:			2016 -12 -04
+// Team Members:	Amshar Basheer, Ben Lorantfy, Grigory Kozyrev, Kyle Stevenson 
+//
+
 #pragma once
 #include<string>
 #include<list>
@@ -7,15 +13,19 @@
 
 using namespace std;
 
+// regex string for valid canadian postal code (no spaces)
 #define POSTAL_REGEX "[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]"  
 
+//class to validate a canadian postal code
+//requires a postal code (N1N1N1) and region (e.g. ON, NT)
 class PostalCodeValidator
 {
 private:
-	string regionCode;
-	string regionName;
-	string postalCode;
-		
+	string regionCode;		//the province/territory
+	string regionName;		//the full name of the province/territory
+	string postalCode;		//the postalcode to validate
+
+							//map to hold rules for each region
 	map<string, pair<string, bool>> codeInfos;
 
 public:
@@ -23,7 +33,10 @@ public:
 
 	PostalCodeValidator(string region, string postalCode);
 
+	//method to populate map with rules for a specific region
 	bool populatecodeInfos();
+
+	//method to validate postal code
 	pair<vector<string>, bool> validatePostalCode();
 
 	virtual ~PostalCodeValidator();
--- Services/checkPostalCheck/checkPostalCheck/main.cpp
+++ Services/checkPostalCheck/checkPostalCheck/main.cpp
@@ -1,14 +1,8 @@
-
-/*
-* Example illustrating HTTP service.
-*
-* Server Usage:
-*    sudo ./distribution/example/http_service
-*
-* Client Usage:
-*    curl -w'\n' -v -X POST --data 'Hello, Restbed' 'http://localhost/resource'
-*/
-
+// Program: SOA
+// Assingment 1
+// Date:			2016 -12 -04
+// Team Members:	Amshar Basheer, Ben Lorantfy, Grigory Kozyrev, Kyle Stevenson 
+//
 #include <memory>
 #include <cstdlib>
 #include <restbed>
@@ -20,89 +14,134 @@
 using namespace std;
 using namespace restbed;
 
+#define PORT 1337
+
 //method to handle get request
 void get_method_handler(const shared_ptr< Session > session)
 {
-	const auto request = session->get_request();
+	try {
 
-	int content_length = 0;
-	request->get_header("Content-Length", content_length);
+		const auto request = session->get_request();
 
-	//get query params for postal code and province if they exist
-	pair<vector<string>, bool> result;
-	string postalCode = request->get_query_parameter("postalcode", "");
-	string province = request->get_query_parameter("province", "");
+		int content_length = 0;
+		request->get_header("Content-Length", content_length);
 
-	//validate the postalcode
-	PostalCodeValidator postalCodeValidator = PostalCodeValidator(province, postalCode);
+		//get query params for postal code and province if they exist
+		pair<vector<string>, bool> result;
+		string postalCode = request->get_query_parameter("postalcode", "");
+		string province = request->get_query_parameter("province", "");
 
-	if (postalCodeValidator.isRegionValid) {
+		//validate the postalcode
+		PostalCodeValidator postalCodeValidator = PostalCodeValidator(province, postalCode);
 
-		result = postalCodeValidator.validatePostalCode();
+		if (postalCodeValidator.isRegionValid) {
 
-		//print out results
-		cout << result.second << "\n";
+			result = postalCodeValidator.validatePostalCode();
 
-		for (int i = 0; i < result.first.size(); i++) {
-			cout << result.first.at(i) << "\n";
+			//print out results
+			cout << result.second << "\n";
+
+			for (int i = 0; i < result.first.size(); i++) {
+				cout << result.first.at(i) << "\n";
+			}
+
+		}
+		else
+		{
+			result.second = false;
+			result.first.push_back("Region not supported");
 		}
 
+		ostringstream convert;
+		ostringstream convert_length;
+		convert << "{\"isValid\": \"" << result.second << "\",";
+		convert << "\"specialNotes\":[";
+
+		for (int i = 0; i < result.first.size(); i++) {
+			convert << "\"" << result.first.at(i) << "\"";
+			if (i < result.first.size() - 1) {
+				convert << ",";
+			}
+		}
+
+		convert << "]}";
+		string json_response = convert.str();
+
+		convert_length << json_response.length();
+		string len = convert_length.str();
+
+		//destroy class
+		//postalCodeValidator.~PostalCodeValidator();
+
+		//send response back to client
+		/*session->fetch(content_length, [](const shared_ptr< Session > session, const Bytes & body)
+		{
+		fprintf(stdout, "%.*s\n", (int)body.size(), body.data());
+		session->close(OK, "Hello, World!", { { "Content-Length", len } });
+		}); */
+
+		//send response back to client
+		session->close(OK, json_response, { { "Content-Length", len } });
+	}
+	catch (exception e) {
+		ostringstream convert;
+		ostringstream convert_length;
+		convert << "{\"isValid\": \"" << 0 << "\",";
+		convert << "\"specialNotes\":[ \"Error validating postal code\" ]}";
+		string json_response = convert.str();
+
+		convert_length << json_response.length();
+		string len = convert_length.str();
+
+		session->close(OK, json_response, { { "Content-Length", len } });
+	}
+}
+
+////method to handle exceptions from resource
+void resource_error_handler(const int, const exception&, const shared_ptr< Session > session)
+{
+	if (session->is_open())
+	{
+		session->close(6000, "Custom Resource Internal Server Error", { { "Content-Length", "37" } });
 	}
 	else
 	{
-		result.second = false;
-		result.first.push_back("invalid Region");
+		fprintf(stderr, "Custom Resource Internal Server Error\n");
 	}
-
-	ostringstream convert;
-	ostringstream convert_length;
-	convert << "{\"isValid\": \"" << result.second << "\",";
-	convert << "\"specialNotes\":[";
-
-	for (int i = 0; i < result.first.size(); i++) {
-		convert << "\"" << result.first.at(i) << "\"";
-		if (i < result.first.size() - 1) {
-			convert << ",";
-		}
-	}
-
-	convert << "]}";
-	string json_response = convert.str();
-
-	convert.clear();
-
-	convert_length << json_response.length();
-	string len = convert_length.str();
-	
-	//destroy class
-	//postalCodeValidator.~PostalCodeValidator();
-
-	//send response back to client
-	/*session->fetch(content_length, [](const shared_ptr< Session > session, const Bytes & body)
-	{
-		fprintf(stdout, "%.*s\n", (int)body.size(), body.data());
-		session->close(OK, "Hello, World!", { { "Content-Length", len } });
-	}); */
-
-	//send response back to client
-	session->close(OK, json_response, { {"Content-Length", len} });
-
-
 }
+
+//method to handle exceptions from service
+void service_error_handler(const int, const exception&, const shared_ptr< Session > session)
+{
+	if (session->is_open())
+	{
+		session->close(5000, "Custom Service Internal Server Error", { { "Content-Length", "36" } });
+	}
+	else
+	{
+		fprintf(stderr, "Custom Service Internal Server Error\n");
+	}
+}
+
 
 int main(const int, const char**)
 {
 	//setup server
-	auto resource = make_shared< Resource >();
-	resource->set_path("/resource");
-	resource->set_method_handler("GET", get_method_handler);
+	cout << "Starting Service [Postal Code Validator]...\n";
+	auto checkPostalCode = make_shared< Resource >();
+	checkPostalCode->set_path("/checkPostalCode");
+	checkPostalCode->set_method_handler("GET", get_method_handler);
+	checkPostalCode->set_error_handler(&resource_error_handler);
 
 	auto settings = make_shared< Settings >();
-	settings->set_port(8080);
+	settings->set_port(PORT);
 	settings->set_default_header("Connection", "close");
 
+	cout << "Service Started\n";
+	cout << "Listening on port:" << PORT;
 	Service service;
-	service.publish(resource);
+	service.publish(checkPostalCode);
+	service.set_error_handler(service_error_handler);
 	service.start(settings);
 
 	return EXIT_SUCCESS;
